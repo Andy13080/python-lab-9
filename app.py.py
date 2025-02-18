bf7eff891be27abdd73d7f05ea408db8eb43e964
Andrew Nkole <andynkole@gmail.com>
2025-02-18 18:01:00 +0300

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hardware_part = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        hardware_part = request.form['hardware_part']
+        price = request.form['price']
+
+        if hardware_part and price:
+            new_expense = Expense(hardware_part=hardware_part, price=float(price))
+            db.session.add(new_expense)
+            db.session.commit()
+            return redirect(url_for('index'))
+
+    expenses = Expense.query.all()
+    total_cost = sum(exp.price for exp in expenses)
+    return render_template('index.html', expenses=expenses, total_cost=total_cost)
+
+if __name__ == '__main__':
+    app.run(debug=True)
